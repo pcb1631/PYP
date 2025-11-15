e9b321c65e492115897a4d80938ef1fb13280e25
pcb1631 <josiah.backup02@gmail.com>
2025-11-15 23:47:17 +0800

    more colours

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ import commands
 #ANSI escape codes for text colors
 RED = '\033[31m'
 GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+BOLD = '\033[1m'
 RESET = '\033[0m'
 
 #globals
