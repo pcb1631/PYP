87eb72c849a2d6460b8733f8e74132d0ad99878f
pcb1631 <josiah.backup02@gmail.com>
2025-11-25 11:08:38 +0800

    reading keyboard symbols should work on linux now

--- kb.py
+++ kb.py
@@ -1,4 +1,7 @@
 import os
+import sys
+import tty
+import termios
 
 def get_key():
     if os.name == 'nt':  # For Windows
@@ -7,14 +10,13 @@ def get_key():
             key = msvcrt.getch()
             return key
     else:  # For macOS and Linux
-        import sys
-        import tty
-        import termios
         fd = sys.stdin.fileno()
         old_settings = termios.tcgetattr(fd)
         try:
             tty.setraw(sys.stdin.fileno())
             key = sys.stdin.read(1)
+            if key == "\x1b":
+                key += sys.stdin.read(2)
             return key
         finally:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings) 
