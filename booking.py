4f3ed25c7b350d1fc0f803338affd32f2b4e3f12
pcb1631 <josiah.backup02@gmail.com>
2025-12-16 08:28:48 +0000

    del

--- booking.py
+++ booking.py
@@ -3,12 +3,10 @@ import json
 from tui import TUI
 from datetime import datetime, timedelta
 
-# Read booking.json
 def load_bookings(filename=files.BOOKING_PATH):
     with open(filename, "r") as booking_file:
         return json.load(booking_file)
 
-# Save bookings into json file
 def save_bookings(data, filename=files.BOOKING_PATH):
     with open(filename, "w") as booking_file:
         json.dump(data, booking_file, indent=4)
@@ -43,26 +41,3 @@ def generate_next_7_days(trainer): # generates 7 days ahead, with 4 slots in eac
                     "booked_by": None
                 }
     save_bookings(bookings)
-
-
-# View booking function for members
-def view_member_bookings(bookings, member_name):
-    print(f"\n{member_name}'s bookings:")
-    found = False
-    for trainer_key, trainer_slots in bookings.items():  # Loop through trainers
-        for slot_id, slot in trainer_slots.items():      # Loop through slots
-            if slot["booked_by"] == member_name:
-                print(f"- Trainer: {trainer_key}, Slot {slot_id}: {slot["date"]} {slot["start"]} - {slot["end"]}")
-                found = True
-
-    if not found:
-        print("No bookings found.")
-
-
-def main():
-    generate_next_7_days("trainer_1")
-
-
-
-if __name__ == "__main__":
-    main()
