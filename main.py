5a13f22cb82dab806e7de916adda88153dad7352
Josiah Hoo <josiah.hoo@gmail.com>
2025-12-23 20:11:25 +0800

    end of file error ??

--- main.py
+++ main.py
@@ -270,7 +270,12 @@ def command_mode():
 
     try:                                # Wrapping the whole thing to catch keyboard interrupts
         while True:
-            command = input(f'[{current_user["user_type"]} {current_user["username"]}@Fitness Center] > ')
+            try:
+                command = input(f'[{current_user["user_type"]} {current_user["username"]}@Fitness Center] > ')
+            except EOFError:
+                print("EOF") # when you go in tui, exit from tui, and then press CTRL+C, this happens.s
+                time.sleep(1)
+                offline()
             command = command.strip()   # remove leading and trailing whitespace
             command = command.split()   # splits each word into a list
             if command == []:
