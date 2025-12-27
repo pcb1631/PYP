a58e3e047ba544a2b2ffa179307ab5f3204b623d
pcb1631 <josiah.backup02@gmail.com>
2025-12-28 03:03:38 +0800

    json saving and loading functions

--- utils.py
+++ utils.py
@@ -22,3 +22,41 @@ def save_bookings(data, filename=files.BOOKING_PATH):
 def epoch_to_readable(timestamp):
     dt = datetime.fromtimestamp(timestamp)
     return dt.strftime("%d/%m/%y %H:%M")
+
+def find(username, filepath):
+    with open(filepath, "r") as f:
+        data = f.read().splitlines()
+    return username in data
+
+
+def load_json_file(filepath):       # generic json loader
+    try:
+        with open(filepath, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print(RED + f"Error: Can't find {filepath}" + RESET)
+        return None
+    except json.JSONDecodeError:
+        print(RED + f"Error: Invalid JSON format in '{filepath}'." + RESET)
+        return None
+    except Exception as e:
+        print(RED + f'Error: {e}' + RESET)
+        return None
+    
+def save_json_file(filepath, data, current_user): # generic json saver
+    username = current_user["username"]
+    
+    if find(username, files.BANNED_PATH):
+        print(RED + "Error: You are banned" + RESET)
+        return False
+    if find(username, files.DELETE_PATH):
+        print(RED + "Error: Your account has been deleted" + RESET)
+        return False
+    
+    try:
+        with open(filepath, "w") as f:
+            json.dump(data, f, indent=4)
+        return True
+    except Exception as e:
+        print(RED + f"Error saving to {filepath}: {e}" + RESET)
+        return False
