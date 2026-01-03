cff4eab80e5c8e5def204bb3cec8b6e83e128174
pcb1631 <josiah.backup02@gmail.com>
2026-01-04 05:22:29 +0800

    new theme :o

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,11 +35,10 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
 html_css_files = ['custom.css']
 
